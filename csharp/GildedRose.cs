e407a7ca5ac7d9cdfcd430968f50da7453d835e6
Claudio López Ardura <cllopez@aidacanarias.com>
2022-01-20 13:18:36 +0100

    ♻ remove backstage item from else

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -65,40 +65,11 @@ namespace csharp
             }
             else
             {
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality > 0)
                 {
-                    if (item.Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality -= 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
+                        item.Quality -= 1;
                     }
                 }
 
@@ -109,19 +80,12 @@ namespace csharp
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality > 0)
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality -= 1;
-                            }
+                            item.Quality -= 1;
                         }
-                    }
-                    else
-                    {
-                        item.Quality -= item.Quality;
                     }
                 }
             }
